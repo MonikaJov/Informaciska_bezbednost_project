cc773e56512784ec23e75cc1dcac80e1f1df4972
Monika Jovevska <monika.jovevska@students.finki.ukim.mk>
2024-01-09 13:35:59 +0100

    Update settings.py

--- IB_proekt/IB_proekt/settings.py
+++ IB_proekt/IB_proekt/settings.py
@@ -23,10 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-d@7aco_u()^96@&5p8lmudj&oa$vkbubuz@17j-%1hd&ow-r62'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False  # Set this to False in production
+# DEBUG = False  # Set this to False in production
+DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
-
+# ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = []
 
 # Application definition
 
@@ -143,12 +144,13 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = '******@gmail.com' # I'm not sharing my email for security reasons
+EMAIL_HOST_USER = '********@gmail.com' # I'm not sharing my email for security reasons
 EMAIL_HOST_PASSWORD = '*************' # I'm not sharing my password for security reasons
 
 LOGIN_URL = 'login_form'
 
+SECURE_SSL_REDIRECT = False
 # Add the following lines to enable SSL
-SECURE_SSL_REDIRECT = True
-SESSION_COOKIE_SECURE = True
-CSRF_COOKIE_SECURE = True
+# SECURE_SSL_REDIRECT = True
+# SESSION_COOKIE_SECURE = True
+# CSRF_COOKIE_SECURE = True
